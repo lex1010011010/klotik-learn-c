256f825493986a2c3c254ca0815cf83fd4b81644
shkuratov.a3 <shkuratov.a3@wb.ru>
2024-08-24 08:49:37 +0300

    add solution

---
+++ solutions/D04/task_01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("here is the solution for the first task\n");
+    return 0;
+}
---
+++ solutions/D04/task_02.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("here is the solution for the second task\n");
+    return 0;
+}
